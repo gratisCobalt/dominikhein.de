b97db75a0339183c1599dddd64564641fcbe49fb
Dominik Hein <dominikhein1@gmx.net>
2025-12-17 17:57:34 +0100

    fix: remove unused motion import

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import { motion } from 'framer-motion'
 import { Hero } from '../components/Hero'
 import { FeaturedProjects } from '../components/FeaturedProjects'
 import { ShopShowcase } from '../components/ShopShowcase'
