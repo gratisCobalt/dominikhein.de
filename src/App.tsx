23362d6c0937c7957c7b646c14e4d400fd3c6c34
Dominik Hein <dominikhein1@gmx.net>
2025-12-19 12:23:43 +0100

    feat: add routes for legal pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { BlogPage } from './pages/BlogPage'
 import BlogPostPage from './pages/BlogPostPage'
+import { ImpressumPage } from './pages/ImpressumPage'
+import { DatenschutzPage } from './pages/DatenschutzPage'
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/blog/:slug" element={<BlogPostPage />} />
+        <Route path="/impressum" element={<ImpressumPage />} />
+        <Route path="/datenschutz" element={<DatenschutzPage />} />
       </Routes>
     </BrowserRouter>
   )
